576ad79d91546b8b359ae1962acb57f82114f920
Xetha666 <bryans27092000@gmail.com>
2025-11-17 16:27:14 -0500

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,5 +17,11 @@ export default defineConfig({
         '@': '/src'
       }
     }
-  }
+  },
+  output : "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [auth()]
 });
